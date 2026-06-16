d937df7bcc567b8f1923cddfb9e9e846713f20d3
GeoBrianHilomen <geohmen@gmail.com>
2026-06-16 11:02:40 +0800

    Day 2 Class Task

---
+++ Day2/sample-project/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TaskListView from './views/Task2/TaskListView.vue';
+
+
+// import Task from "./components/Task.vue";
+// import Task1 from "./views/Task1.vue";
+</script>
+
+<template>
+  <!-- <Task1 /> -->
+  <TaskListView />
+</template>
